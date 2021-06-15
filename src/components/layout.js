84e8663ba57589914cbde64f073700deb1bb05a7
Takaichi Daisuke <8767154@gmail.com>
2021-06-16 07:33:24 +0900

    DOM関連エラー回避

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'gatsby'
 import Container from './container'
 import Navigation from './navigation'
@@ -7,16 +7,18 @@ import '../css/base.scss'
 const Template = ({ location, children }) => {
     let header
 
-    ((w, d) => {
-      let id='embedly-platform', n = 'script';
-      if (!d.getElementById(id)){
-        w.embedly = w.embedly || function() {(w.embedly.q = w.embedly.q || []).push(arguments);};
-        let e = d.createElement(n); e.id = id; e.async=1;
-        e.src = ('https:' === document.location.protocol ? 'https' : 'http') + '://cdn.embedly.com/widgets/platform.js';
-        let s = d.getElementsByTagName(n)[0];
-        s.parentNode.insertBefore(e, s);
-      }
-     })(window, document);
+    useEffect(() => {
+      ((w, d) => {
+        let id='embedly-platform', n = 'script';
+        if (!d.getElementById(id)){
+          w.embedly = w.embedly || function() {(w.embedly.q = w.embedly.q || []).push(arguments);};
+          let e = d.createElement(n); e.id = id; e.async=1;
+          e.src = ('https:' === document.location.protocol ? 'https' : 'http') + '://cdn.embedly.com/widgets/platform.js';
+          let s = d.getElementsByTagName(n)[0];
+          s.parentNode.insertBefore(e, s);
+        }
+       })(window, document);
+    })
 
     let rootPath = `/`
     if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
